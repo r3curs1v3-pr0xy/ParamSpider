0fb9c545d3cbc7b4a250d748265061af8d4b6374
r3curs1v3-pr0xy <55408209+r3curs1v3-pr0xy@users.noreply.github.com>
2021-03-13 00:49:34 +0530

    Changes as per Vajra's requirements

--- core/extractor.py
+++ core/extractor.py
@@ -1,32 +1,30 @@
-import re 
+import re
 
 
-def param_extract(response, level, black_list, placeholder):
+def param_extract(response, level, black_list):
+    # '''
+    # regexp : r'.*?:\/\/.*\?.*\=[^$]'
+    # regexp : r'.*?:\/\/.*\?.*\='
+    # '''
 
-    ''' 
-    Function to extract URLs with parameters (ignoring the black list extention)
-    regexp : r'.*?:\/\/.*\?.*\=[^$]'
-    
-    '''
-
-    parsed = list(set(re.findall(r'.*?:\/\/.*\?.*\=[^$]' , response)))
+    parsed = list(set(re.findall(r'.*?:\/\/.*\?.*\=*', response)))
     final_uris = []
-        
+
     for i in parsed:
         delim = i.find('=')
         second_delim = i.find('=', i.find('=') + 1)
         if len(black_list) > 0:
             words_re = re.compile("|".join(black_list))
             if not words_re.search(i):
-                final_uris.append((i[:delim+1] + placeholder))
+                final_uris.append((i))
                 if level == 'high':
-                    final_uris.append(i[:second_delim+1] + placeholder)
+                    final_uris.append(i)
         else:
-            final_uris.append((i[:delim+1] + placeholder))
+            final_uris.append((i))
             if level == 'high':
-                final_uris.append(i[:second_delim+1] + placeholder)
+                final_uris.append(i)
 
     # for i in final_uris:
     #     k = [ele for ele in black_list if(ele in i)]
-    
+
     return list(set(final_uris))
